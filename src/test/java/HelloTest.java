dbbb87e78411443ca03d112b66f0a4cf96768c24
Ismail Ozden <ismailozden100@gmail.com>
2021-02-12 22:58:08 +0100

    some line added in master

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("some line");
         System.out.println("some line from movie1 branch");
+
+        //master
     }
 }
