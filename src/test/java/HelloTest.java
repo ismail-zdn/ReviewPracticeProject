0df880abd598e2e0881aef2a220b25b6471fab21
Ismail Ozden <ismailozden100@gmail.com>
2021-02-12 22:31:50 +0100

    some line added in movie1 branch

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,6 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("some line");
+        System.out.println("some line from movie1 branch");
     }
 }
